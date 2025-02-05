b4476779cceda4c60a3c283aa2b7a68900167cb7
kadetov18 <kadetovolegosnovnai@gmail.com>
2025-02-05 22:26:45 +0200

    add-user-id-and-authentication-jwt

--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -3,9 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { BookController } from './book.controller';
 import { BookService } from './book.service';
 import { Book, BookSchema } from './schemas/book.schema';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
+    AuthModule,
     MongooseModule.forFeature([{ name: Book.name, schema: BookSchema }]),
   ],
   controllers: [BookController],
